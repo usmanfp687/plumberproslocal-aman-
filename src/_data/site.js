7c2944cf87f76d0830b4dfd9071705571e1e971a
usmanfp687 <usmanfp143@gmail.com>
2026-07-02 02:39:16 +0500

    Update phone number across site

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,7 @@
 // Global site constants — available as {{ site.PHONE }}, {{ site.BRAND }}, etc. in all templates
 export default {
-  PHONE: '(877) 710-2942',
-  PHONE_HREF: 'tel:+18777102942',
+  PHONE: '(877) 765-9415',
+  PHONE_HREF: 'tel:+18777659415',
   BRAND: 'Plumbing Local Pros',
   DOMAIN: 'https://www.plumbinglocalpros.com',
   YEAR: new Date().getFullYear(),
